3892a29f8be4cd49fcb1b2bdc97164b615b676e1
arya-raditya <aryaraditya.kusuma2006@gmail.com>
2025-02-19 14:03:49 +0700

    feat: add unit test for EShopApplication

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,13 +1,26 @@
 package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class EshopApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        assertNotNull(applicationContext, "Application context should load successfully");
+    }
+
+    @Test
+    void mainMethodRunsSuccessfully() {
+        EshopApplication.main(new String[] {}); // Run main method.
     }
 
 }
