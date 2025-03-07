59b7680a21d9359ffd613bf3ed2cb43a5990923a
arya-raditya <aryaraditya.kusuma2006@gmail.com>
2025-03-07 18:57:12 +0700

    [RED] Create skeleton for PaymentVoucher

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentVoucher.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import java.util.Map;
+
+public class PaymentVoucher {
+    public static boolean validatePaymentData(Map<String, String> paymentData) {
+        // skeleton placeholder
+        return false;
+    }
+}
